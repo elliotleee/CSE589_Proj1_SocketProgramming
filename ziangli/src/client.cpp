8b9de24840cd683e4fbd69283cc6309c55612a2e
Lei Chen <chenlei9691@gmail.com>
2018-10-10 18:10:36 -0400

    short version of get ip address

--- ziangli/src/client.cpp
+++ ziangli/src/client.cpp
@@ -4,6 +4,7 @@
 #include <string>
 // #include <string.h>
 #include <arpa/inet.h> // 提供IP地址转换函数
+#include <ifaddrs.h>
 
 #include "../include/global.h"
 #include "../include/logger.h"
@@ -13,30 +14,35 @@
 #define MAXSIZE 100 // max size of bytes transfer
 #include<iostream>
 
-string getIPAddress(){
-    string ipAddress="Unable to get IP Address";
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    int success = 0;
-    // retrieve the current interfaces - returns 0 on success
-    success = getifaddrs(&interfaces);
-    if (success == 0) {
-        // Loop through linked list of interfaces
-        temp_addr = interfaces;
-        while(temp_addr != NULL) {
-            if(temp_addr->ifa_addr->sa_family == AF_INET) {
-                // Check if interface is en0 which is the wifi connection on the iPhone
-                ipAddress=inet_ntoa(((struct sockaddr_in*)temp_addr->ifa_addr)->sin_addr);
-                printf(ipAddress)
-            }
-            temp_addr = temp_addr->ifa_next;
-        }
-    }
-    // Free memory
-    freeifaddrs(interfaces);
-    return ipAddress;
+// string getIPAddress(){
+//     string ipAddress="Unable to get IP Address";
+//     struct ifaddrs *interfaces = NULL;
+//     struct ifaddrs *temp_addr = NULL;
+//     int success = 0;
+//     // retrieve the current interfaces - returns 0 on success
+//     success = getifaddrs(&interfaces);
+//     if (success == 0) {
+//         // Loop through linked list of interfaces
+//         temp_addr = interfaces;
+//         while(temp_addr != NULL) {
+//             if(temp_addr->ifa_addr->sa_family == AF_INET) {
+//                 // Check if interface is en0 which is the wifi connection on the iPhone
+//                 ipAddress=inet_ntoa(((struct sockaddr_in*)temp_addr->ifa_addr)->sin_addr);
+//                 printf(ipAddress)
+//             }
+//             temp_addr = temp_addr->ifa_next;
+//         }
+//     }
+//     // Free memory
+//     freeifaddrs(interfaces);
+//     return ipAddress;
+// }
+
+void *get_in_addr(struct sockaddr *sa)
+{
+	return &(((struct sockaddr_in*)sa)->sin_addr);
 }
 
 int main(){
-    getIPAddress()
+    cout << get_in_addr()
 }
